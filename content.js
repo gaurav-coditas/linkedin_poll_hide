4cdfcae6f99bc09ea05d9d5317c49db5cd04ab73
Suraj Mishra <suraj260596@gmail.com>
2021-05-28 18:40:55 +0530

    Extension revamp

--- content.js
+++ content.js
@@ -1,4 +1,19 @@
-setInterval(() => { 
-    console.log('hiding polls')
-    Array.from(document.getElementsByClassName('feed-shared-poll')).forEach(item => { item.parentElement.style.display = 'none'; }) 
-}, 3000)
+let observer = new MutationObserver((entries) => {
+    entries.forEach((e) => {
+        e.target.classList.forEach(item => {
+            if (item === "feed-shared-poll") {
+                let item = document.querySelector(".feed-shared-poll");
+                if (item) {
+                    item.remove();
+                }
+            }
+        });
+    })
+})
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+    attributes: false,
+    characterData: false
+})
